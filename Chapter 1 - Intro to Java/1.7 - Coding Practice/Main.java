4665f7cf02d4c17bbad181d1f91f2627434d52b0
Dalton Carter <s-dcarter@lwsd.org>
2023-09-18 08:37:51 -0700

    Lab 1.7

--- Chapter 1 - Intro to Java/1.7 - Coding Practice/Main.java
+++ Chapter 1 - Intro to Java/1.7 - Coding Practice/Main.java
@@ -17,26 +17,75 @@ public class Main {
     }
 
     public static void exercise5() {
-        // TODO: Your code for this exercise goes here
+        System.out.println("A \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty String.");
+        System.out.println("Don't forget: use \\\" instead of \" !");
+        System.out.println("'' is not the same as \"");
     }
 
     public static void exercise6() {
-        // TODO: Your code for this exercise goes here
+        System.out.println("public class Main {");
+        System.out.println("    public static void main(String[] args) {");
+        System.out.println("        System.out.println(\"Hello World!\");");
+        System.out.println("    }");
+        System.out.println("}");
     }
 
     public static void exercise7() {
-        // TODO: Your code for this exercise goes here
+        printExercise7Text();
+        System.out.println();
+        printExercise7Text();
+    }
+    static void printExercise7Text() {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
     }
 
     public static void exercise8() {
-        // TODO: Your code for this exercise goes here
+        printExercise8Top();
+        for (int i = 0; i < 5; i++) {
+            printExercise8Middle();
+            printExercise8Bottom();
+        }
+    }
+    static void printExercise8Top() {
+        System.out.println("//////////////////////");
+    }
+    static void printExercise8Middle() {
+        System.out.println("|| Victory is mine! ||");
+    }
+    static void printExercise8Bottom() {
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
     }
 
     public static void exercise9() {
-        // TODO: Your code for this exercise goes here
+        printHexTop();
+        printQuoteLine();
+        printHexBottom();
+    }
+
+    static void printHex() {
+        printHexTop();
+        printHexBottom();
+    }
+    static void printHexTop() {
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+    }
+    static void printHexBottom() {
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+    }
+    static void printQuoteLine() {
+        System.out.println("-\"-'-\"-'-\"-");
     }
 
     public static void exercise10() {
-        // TODO: Your code for this exercise goes here
+        printHex();
+        printQuoteLine();
+        printHex();
     }
 }
