262855712e60cdd8b61b40e3aaa3b26e87113bfd
Dalton Carter <s-dcarter@lwsd.org>
2023-10-10 09:14:15 -0700

    3.2b Lab

--- Chapter 3 - Methods/3.2b - Method Versatility/AppMain.java
+++ Chapter 3 - Methods/3.2b - Method Versatility/AppMain.java
@@ -1,21 +1,29 @@
 public class AppMain {
     public static void main(String[] args) {
         // Line 1
-        // TODO
+        for (int i = 0; i < 5; i++) {
+            System.out.print(randInt(100) + " ");
+        }
+        System.out.println();
 
         // Line 2
-        // TODO
+        for (int i = 0; i < 5; i++) {
+            System.out.print(randInt(100, 500) + " ");
+        }
+        System.out.println();
     }
     
     // Put your functions here...
     // Function 1 should: return a value with a maximum of a provided value, [0,𝑚𝑎𝑥𝑉𝑎𝑙𝑢𝑒).
     // Function 2 should: should return a value between two provided doubles, [𝑚𝑖𝑛𝑉𝑎𝑙𝑢𝑒,𝑚𝑎𝑥𝑉𝑎𝑙𝑢𝑒).
 
-    static double max(double num, double max) {
-        return Math.min(num, max);
+    public static int randInt(int maxValue) {
+        return randInt(0, maxValue);
     }
 
-    static double clamp(double num, double min, double max) {
-        return Math.max(Math.min(num, max), min);
+    public static int randInt(int minValue, int maxValue) {
+        if (maxValue < minValue) { throw new IllegalArgumentException("maxValue must be larger than minValue!"); }
+        int range = maxValue - minValue; // the range of the random number
+        return (int)(Math.random() * range) + minValue;
     }
 }
