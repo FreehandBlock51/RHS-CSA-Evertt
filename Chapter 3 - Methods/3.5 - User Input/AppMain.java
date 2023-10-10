55f300e6995a8f8dbcbc798c9eb6ac786dad4591
Dalton Carter <s-dcarter@lwsd.org>
2023-10-10 09:14:23 -0700

    3.5

--- Chapter 3 - Methods/3.5 - User Input/AppMain.java
+++ Chapter 3 - Methods/3.5 - User Input/AppMain.java
@@ -1,7 +1,29 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
         // Create a Scanner object.
         // Asks the user to type their age, the current year, a future year, and their name.
         // Print out "<Name> will be <computed age> years old in <future year>".
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("What is your name? ");
+            String name = scanner.nextLine();
+
+            System.out.print("What is your current age? ");
+            int curAge = scanner.nextInt();
+
+            System.out.print("What year are you trying to calculate your age at? ");
+            int targetYear = scanner.nextInt();
+
+            Calendar cal = Calendar.getInstance();
+            int curYear = cal.get(Calendar.YEAR);
+            int yearsUntilTarget = targetYear - curYear;
+
+            int targetAge = curAge + yearsUntilTarget;
+            System.out.printf("%s will be %d years old in %d\n", name, targetAge, targetYear);
+        }
     }
 }
