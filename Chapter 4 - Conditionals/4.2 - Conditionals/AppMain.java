259a6506d5ff9c8d5b01f8fa1ef524eba3e3cb8e
Dalton Carter <s-dcarter@lwsd.org>
2023-10-12 08:44:54 -0700

    [4.2] use println instead of assert

--- Chapter 4 - Conditionals/4.2 - Conditionals/AppMain.java
+++ Chapter 4 - Conditionals/4.2 - Conditionals/AppMain.java
@@ -4,11 +4,22 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("-------- firstStars --------");
         // add your test cases here
+        System.out.println("(\"**\" -> true) " + (Conditionals.firstStars("**") == true));
+        System.out.println("(\"*-\" -> false) " + (Conditionals.firstStars("*-") == false));
+        System.out.println("(\"***abc\" -> true) " + (Conditionals.firstStars("***abc") == true));
+        System.out.println("(\"**a*\" -> false) " + (Conditionals.firstStars("**a*") == false));
 
         System.out.println("------ compareDouble -------");
         // add your test cases here
+        assert Conditionals.compareDouble(6.001, 6) == true;
+        assert Conditionals.compareDouble(6.011, 6) == false;
+        assert Conditionals.compareDouble(-1.1, -1) == false;
 
         System.out.println("-------- logicCheck --------");
         // add your test cases here
+        assert Conditionals.logicCheck(15, 20, false) == true;
+        assert Conditionals.logicCheck(9, 7, true) == true;
+        assert Conditionals.logicCheck(10, 20, false) == false;
+        assert Conditionals.logicCheck(5, 20, true) == false;
     }
 }
