e9e6cd7dfc25a34d2522231fa80baae6c0e1e749
Dalton Carter <s-dcarter@lwsd.org>
2023-10-19 09:22:37 -0700

    4.4

--- Chapter 4 - Conditionals/4.4 - Cumulative Algorithms/AppMain.java
+++ Chapter 4 - Conditionals/4.4 - Cumulative Algorithms/AppMain.java
@@ -2,11 +2,18 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("-------- alphabet --------");
         // add your test cases here
+        System.out.println("alphabet('a', 5) -> \"abcde\": " + (Cumulative.alphabet('a', 5).contentEquals("abcde")));
+        System.out.println("alphabet('c', 4) -> \"cdef\": " + (Cumulative.alphabet('c', 4).contentEquals("cdef")));
+        System.out.println("alphabet('y', 6) -> \"yzabcd\": " + (Cumulative.alphabet('y', 6).contentEquals("yzabcd")));
 
         System.out.println("------- factorSum --------");
         // add your test cases here
+        System.out.println("factorSum(6) -> 6: " + (Cumulative.factorSum(6) == 6));
+        System.out.println("factorSum(14) -> 10: " + (Cumulative.factorSum(14) == 10));
 
         System.out.println("------- isPerfect --------");
         // add your test cases here
+        System.out.println("isPerfect(6) -> true: " + (Cumulative.isPerfect(6) == true));
+        System.out.println("isPerfect(14) -> false: " + (Cumulative.isPerfect(14) == false));
     }
 }
--- Chapter 4 - Conditionals/4.4 - Cumulative Algorithms/Cumulative.java
+++ Chapter 4 - Conditionals/4.4 - Cumulative Algorithms/Cumulative.java
@@ -1,5 +1,5 @@
 public class Cumulative {
-    public String alphabet(char first, int count) {
+    public static String alphabet(char first, int count) {
         // Return a String consiting of a section of the alphabet.
         //  It should start with letter 'first' and contain 'count' letters.
         //  If count goes past 'z', then it should wrap back around to 'a' 
@@ -10,26 +10,47 @@ public class Cumulative {
         //  alphabet('a', 5) -> "abcde"
         //  alphabet('c', 4) -> "cdef"
         //  alphabet('y', 6) -> "yzabcd"
-        return "abc...";
+        StringBuilder builder = new StringBuilder(count);
+        
+        final int ALPHABET_LENGTH = 26;
+        final int remainingLetters = ALPHABET_LENGTH - (first - 'a');
+        
+        for (int i = 0; i < count; i++) {
+            if (i > remainingLetters) {
+                builder.append((char)('a' + ((i - remainingLetters) % ALPHABET_LENGTH)));
+            }
+            else {
+                builder.append((char)(first + i));
+            }
+        }
+
+        return builder.toString();
     }
 
-    public int factorSum(int n) {
+    public static int factorSum(int n) {
         // Returns the sum of all factors of n, except for n itself
         
         // Examples...
         //  factorSum(6) -> 6      // 1 + 2 + 3
         //  factorSum(14) -> 10    // 1 + 2 + 7
-        return 0;
+        int sum = 0;
+        for (int factor = 1; factor < n; factor++) {
+            if (n % factor == 0) {
+                sum += factor;
+            }
+        }
+        return sum;
     }
 
-    public boolean isPerfect(int n) {
+    public static boolean isPerfect(int n) {
         // Returns true if n is a perfect number, false otherwise
         //  A perfect number is a positive integer that is equal to the sum of its proper divisors.
         //  Use the factorSum function you wrote to help you do this (and do not copy the code).
         
         // Examples...
-        //  factorSum(6) -> true   // 1 + 2 + 3 = 6, so it is a perfect number
-        //  factorSum(14) -> false // 1 + 2 + 7 = 10, which is not 14, so not perfect
-        return false;
+        //  isPerfect(6) -> true   // 1 + 2 + 3 = 6, so it is a perfect number
+        //  isPerfect(14) -> false // 1 + 2 + 7 = 10, which is not 14, so not perfect
+        if (n <= 0) { return false; }
+        return n == factorSum(n);
     }
 }
