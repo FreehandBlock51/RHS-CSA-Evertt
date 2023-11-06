56c3f38adffe1787178ef86f2a59dbae5ae40e35
Dalton Carter <s-dcarter@lwsd.org>
2023-11-06 08:54:41 -0800

    numbers for calculator

--- Chapter 4 - Conditionals/4.x - Calculator/student/Calculator.java
+++ Chapter 4 - Conditionals/4.x - Calculator/student/Calculator.java
@@ -11,16 +11,91 @@ public class Calculator {
      * 
      * @param buttonName The name of the button that was clicked. This should always match one String from the {@link Buttons} class in Buttons.java.
      */
-    public static void handleButtonClick(String buttonName){
-        if (buttonName.equals(Buttons.ONE)) {
-            //TODO Any conditions to check for when '1' is clicked? Any other things you should update?
-            //Use statically imported methods from the EngineAPI.java file:
-            appendDigitToEntry('1');
-            setEntryIncomplete(true);
+    public static void handleButtonClick(String buttonName) {
+        if (wasPreviousButtonEquals()) {
+            clearExpression();
+            setEntry("");
             setWasPreviousButtonEquals(false);
         }
-        //TODO Handle as many buttons as possible! See the list of Buttons constants in the Buttons.java class.
-        //Whenever you find yourself repeating code, you may want to create other static methods in this class to help!
+        switch (buttonName) {
+            case Buttons.CLEAR_ALL:
+                clearExpression();
+                clearPreviousOperationButton();
+                clearPreviousResult();
+            case Buttons.CLEAR_ENTRY:
+                setEntry(0);
+                break;
+
+            case Buttons.ONE:
+            case Buttons.TWO:
+            case Buttons.THREE:
+            case Buttons.FOUR:
+            case Buttons.FIVE:
+            case Buttons.SIX:
+            case Buttons.SEVEN:
+            case Buttons.EIGHT:
+            case Buttons.NINE:
+            case Buttons.ZERO:
+                if ((getEntryAsString().equals("0") && !entryHasADecimalPoint()) || !isEntryIncomplete()) {
+                    setEntry(buttonName);
+                }
+                else {
+                    appendDigitToEntry(buttonName);
+                }
+                setEntryIncomplete(true);
+                setWasPreviousButtonEquals(false);
+                break;
+            
+            case Buttons.DECIMAL_POINT:
+                if (!entryHasADecimalPoint()) {
+                    appendDecimalPointToEntry();
+                }
+                break;
+            
+            case Buttons.PLUS:
+            case Buttons.MINUS:
+            case Buttons.MULTIPLIED_BY:
+            case Buttons.DIVIDED_BY:
+                appendToExpression(getEntryAsString() + buttonName);
+                setPreviousResult(getEntryAsDouble());
+                setPreviousOperationButton(buttonName);
+                setEntry("0");
+                setEntryIncomplete(false);
+                break;
+            
+            case Buttons.EQUALS:
+                appendToExpression(getEntryAsString());
+                setWasPreviousButtonEquals(true);
+                setEntryIncomplete(false);
+                double result = 0;
+                switch (getPreviousOperationButton()) {
+                    case Buttons.PLUS:
+                        result = getPreviousResult() + getEntryAsDouble();
+                        break;
+                    case Buttons.MINUS:
+                        result = getPreviousResult() - getEntryAsDouble();
+                        break;
+                    case Buttons.MULTIPLIED_BY:
+                        result = getPreviousResult() * getEntryAsDouble();
+                        break;
+                    case Buttons.DIVIDED_BY:
+                        result = getEntryAsDouble() / getEntryAsDouble();
+                        break;
+                    
+                    case "":
+                        result = getEntryAsDouble();
+                        break;
+                
+                    default:
+                        break;
+                }
+                setEntry(result);
+                setPreviousResult(result);
+                break;
+        
+            default:
+                break;
+        }
     }
 
 }
