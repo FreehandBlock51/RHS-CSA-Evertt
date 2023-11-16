41693de976692db04fddd0a91f939b95a6b8eb12
Dalton Carter <s-dcarter@lwsd.org>
2023-11-16 09:09:08 -0800

    While Loops lab

--- Chapter 5 - Coding/5.1 - While Loops/AppMain.java
+++ Chapter 5 - Coding/5.1 - While Loops/AppMain.java
@@ -1,3 +1,26 @@
 public class AppMain {
-    // Your code here...
+    public static void main(String[] args) {
+        int n = 1, count = 0;
+        while (count < 20) {
+            if (isPrime(n)) {
+                System.out.println(n + " is prime");
+                count++;
+            }
+            if (++n < 0) {
+                System.err.println("FAIL: Integer Overflow!");;
+                break;
+            }
+        }
+        System.out.println("Found " + count + " primes!");
+    }
+
+    static boolean isPrime(int n) {
+        n = Math.abs(n);
+        for (int factor = 2; factor < n; factor++) {
+            if (n % factor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
