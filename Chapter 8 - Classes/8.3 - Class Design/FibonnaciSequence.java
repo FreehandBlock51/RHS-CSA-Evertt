eaad73e62e60b7942640ec3c4b63f137f8e97c73
Dalton Carter <s-dcarter@lwsd.org>
2023-12-07 09:48:27 -0800

    did fib seq with for loop

--- Chapter 8 - Classes/8.3 - Class Design/FibonnaciSequence.java
+++ Chapter 8 - Classes/8.3 - Class Design/FibonnaciSequence.java
@@ -1,8 +1,21 @@
-public class FibonnaciSequence extends CachedSequence {
+public class FibonnaciSequence extends Sequence {
     private static final double[] FIRST_TERMS = new double[] {0, 1};
 
-    public FibonnaciSequence() {
-        super(5);
+    @Override
+    public double getTermAt(int index) {
+        if (index == 0) {
+            return FIRST_TERMS[0];
+        }
+        double cur = FIRST_TERMS[1],
+         prev = FIRST_TERMS[0],
+         tmp;
+
+        for (int i = 1; i < index; i++) {
+            tmp = cur;
+            cur += prev;
+            prev = tmp;
+        }
+        return cur;
     }
 
     @Override
