a85d3398de802ff675bdfe5b657d88691a55aca3
FreehandBlock51 <freehandblock51@outlook.com>
2024-02-23 08:46:36 -0800

    10.2 lab

--- Chapter 10 - ArrayLists/10.2 - ArrayLists/AppMain.java
+++ Chapter 10 - ArrayLists/10.2 - ArrayLists/AppMain.java
@@ -1,17 +1,90 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
+
 public class AppMain {
+    private static void assertThat(boolean bool, String errMsg) {
+        assert bool;
+        
+        if (!bool) {
+            throw new AssertionError(errMsg);
+        }
+    }
+
+    private static void assertEq(Object a, Object b) {
+        assertThat(Objects.equals(a, a), "Objects.equals(" + Objects.toString(a) + ", " + Objects.toString(a) + ") failed! (a != a)");
+        assertThat(Objects.equals(b, b), "Objects.equals(" + Objects.toString(b) + ", " + Objects.toString(b) + ") failed! (b != b)");
+        assertThat(Objects.equals(a, b), "Objects.equals(" + Objects.toString(a) + ", " + Objects.toString(b) + ") failed! (a != b)");
+        assertThat(Objects.equals(b, a), "Objects.equals(" + Objects.toString(b) + ", " + Objects.toString(a) + ") failed! (b != a)");
+
+        System.out.println("assertion " + Objects.toString(a) + " == " + Objects.toString(b) + " passed!");
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static void assertIterablesAreEqual(Iterable a, Iterable b) {
+        System.out.println("Testing that " + a + " has the same items as " + b + "...");
+        Iterator aIter = a.iterator();
+        Iterator bIter = b.iterator();
+        while (aIter.hasNext() & bIter.hasNext()) {
+            assertEq(aIter.next(), bIter.next());
+        }
+        assertThat(!aIter.hasNext() & !bIter.hasNext(), "Iterators " + a.toString() + " and " + b.toString() + " are of different lengths!");
+        System.out.println("Test " + a + " is equivalent to " + b + " passed!");
+    }
+
+    private static class ArrayIterable<T> implements Iterable<T> {
+        private final T[] arr;
+        public ArrayIterable(T[] arr) {
+            this.arr = arr;
+        }
+        @Override
+        public Iterator<T> iterator() {
+            return Arrays.stream(arr).iterator();
+        }
+        @Override
+        public String toString() {
+            return Arrays.toString(arr);
+        }
+    }
+
+    private static <T> void assertArrayListEqualsArray(ArrayList<T> list, T[] arr) {
+        assertIterablesAreEqual(list, new ArrayIterable<>(arr));
+    }
+
+    private static <T> void testToArrayList(T[] arr) {
+        ArrayList<T> list = Practice.toArrayList(arr);
+        System.out.println("Testing " + Arrays.toString(arr) + " == " + list.toString() + " ...");
+        assertEq(list.size(), arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            assertEq(list.get(i), arr[i]);
+        }
+    }
+
     public static void main(String[] args) {
         Practice practice = new Practice();
 
         System.out.println("-------- toArrayList --------");
-        // TODO: add your test cases here
+        testToArrayList(new String[] {"ewfesd", "asdz", "3"});
+        testToArrayList(new String[] {"a", "b", "c"});
+        testToArrayList(new String[0]);
+        assertEq(Practice.toArrayList(null), null);
 
         System.out.println("\n-------- reverseList --------");
-        // TODO: add your test cases here
+        assertArrayListEqualsArray(Practice.reverseList(new String[] {"a", "b", "c"}), new String[] { "c", "b", "a" });
+        assertArrayListEqualsArray(Practice.reverseList(new String[] {""}), new String[] {""});
+        assertEq(Practice.reverseList(null), null);
 
         System.out.println("\n-------- splitString --------");
-        // TODO: add your test cases here
+        assertArrayListEqualsArray(Practice.splitString("This is a test"), new String[] {"This", "is", "a", "test"});
+        assertArrayListEqualsArray(Practice.splitString("Hi. What, is your name?"), new String[] { "Hi", "What", "is", "your", "name" });
+        assertEq(Practice.splitString(null), null);
 
         System.out.println("\n-------- justPrimes --------");
-        // TODO: add your test cases here
+        assertArrayListEqualsArray(Practice.justPrimes(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 89}), new Integer[] {2, 3, 5, 7, 89});
+        assertArrayListEqualsArray(Practice.justPrimes(new int[0]), new Integer[0]);
+        assertArrayListEqualsArray(Practice.justPrimes(new int[] {1,6,4,10}), new Integer[0]);
+        assertArrayListEqualsArray(Practice.justPrimes(new int[] {1,3,5,-7,-1,13}), new Integer[] {3,5,13});
+        assertEq(Practice.justPrimes(null), null);
     }
 }
--- Chapter 10 - ArrayLists/10.2 - ArrayLists/Practice.java
+++ Chapter 10 - ArrayLists/10.2 - ArrayLists/Practice.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Practice {
     /* Write a method called toArrayList that takes an array of Strings and
      *  return an ArrayList populated with the same list of Strings.
      * Example...
      *  input: { "a", "b", "c" }, output: [ a, b, c ]
-     */ 
-    // TODO: write the method toArrayList
+     */
+    public static <T> ArrayList<T> toArrayList(T[] arr) {
+        return arr == null ? null : new ArrayList<>(Arrays.asList(arr));
+    }
 
     /* Write a method called reverseList that takes an array of Strings and
      *  return a new ArrayList with the same strings, but with their locations
@@ -12,7 +17,16 @@ public class Practice {
      * Example...
      *  input: { "a", "b", "c" }, output: [ c, b, a ]
      */ 
-    // TODO: write the method reverseList
+    public static <T> ArrayList<T> reverseList(T[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        ArrayList<T> list = new ArrayList<>(arr.length);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
 
     /* Write a method called splitString that takes a String and returns an
      *  ArrayList with each word in the String as an element of the ArrayList.
@@ -23,12 +37,63 @@ public class Practice {
      *  input: "This is a test", output: [ this, is, a, test ]
      *  input: "Hi. What, is your name?", output: [ Hi, What, is, your, name ]
      */ 
-    // TODO: write the method splitString
+    public static ArrayList<String> splitString(String str) {
+        if (str == null) {
+            return null;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        int startIdx;
+        int endIdx = -1;
+        while (endIdx < str.length()) {
+            startIdx = endIdx + 1;
+            endIdx = str.indexOf(" ", startIdx);
+            if (endIdx == -1) {
+                endIdx = str.length();
+            }
+
+            String sub = str.substring(startIdx, endIdx);
+            if (sub.length() == 0) {
+                continue;
+            }
+            char last = sub.charAt(sub.length() - 1);
+            switch (last) {
+                case '!':
+                case '?':
+                case '.':
+                case ',':
+                    list.add(sub.substring(0, sub.length() - 1));
+                    break;
+                default:
+                    list.add(sub);
+            }
+        }
+        return list;
+    }
 
     /* Write a method called justPrimes that takes an Array of integer types
      *  and returns an ArrayList of the subset of those numbers that are prime.
      * Examples...
      *  input: { 1, 3, 4, 7, 8, 11 }, output: [ 1, 3, 7, 11 ]
      */ 
-    // TODO: write the method justPrimes
+    public static ArrayList<Integer> justPrimes(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        ArrayList<Integer> list = new ArrayList<>(arr.length);
+        outer:
+        for (int n : arr) {
+            if (n <= 1) { // 1, 0, and negatives are not prime
+                continue outer;
+            }
+
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    continue outer;
+                }
+            }
+
+            list.add(n);
+        }
+        return list;
+    }
 }
