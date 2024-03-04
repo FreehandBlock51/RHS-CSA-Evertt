eba6b99bc82774303d1bb4f8970c0550447d1f4c
FreehandBlock51 <freehandblock51@outlook.com>
2024-03-04 08:56:32 -0800

    first draft

--- Chapter 8 - Classes/tanks_v2/ai/TankAI.java
+++ Chapter 8 - Classes/tanks_v2/ai/TankAI.java
@@ -3,15 +3,16 @@ package ai;
 import game.PowerUp;
 import game.Tank;
 import game.TankAIBase;
+import game.Target;
 import game.Vec2;
 
 public class TankAI extends TankAIBase {
 
     public String getPlayerName() {
-        return "YOUR-NAME-HERE";  // <---- Put your first name here
+        return "Dalton";  // <---- Put your first name here
     }
     public int getPlayerPeriod() {
-        return -1;                // <---- Put your period here
+        return 1;                // <---- Put your period here
     }
         
     // You are free to add member variables & methods to this class (and delete this comment).
@@ -21,8 +22,40 @@ public class TankAI extends TankAIBase {
     //  teacher if you are not sure. If it feels like cheating, it probably is.
 
     public boolean updateAI() {
+        if (tank.hasCommand()) {
+            return false;
+        }
 
-        // TODO: Your code goes here
+        Target t = null;
+
+        for (Target target : getTargets()) {
+            if (target.getPos().distance(getTankPos()) > getTankShotRange()) {
+                continue;
+            }
+            if (t == null || t.getPos().distance(getTankPos()) > target.getPos().distance(getTankPos())) {
+                t = target;
+            }
+        }
+        if (t != null) {
+            queueCmd("shoot", t.getPos().subtract(getTankPos()));
+            return true;
+        }
+
+        PowerUp powerUp = getPowerUp();
+
+        for (PowerUp p : getPowerUps()) {
+            if (powerUp.getType().equals("p") && !p.getType().equals("p")) {
+                continue;
+            }
+            if (p.getType().equals("p") || p.getPos().distance(getTankPos()) < powerUp.getPos().distance(getTankPos())) {
+                powerUp = p;
+            }
+        }
+
+        final Vec2 dist = powerUp.getPos().subtract(getTankPos());
+
+        queueCmd("move", Vec2.right().multiply(dist.x));
+        queueCmd("move", Vec2.up().multiply(dist.y));
 
         return true;
     }
