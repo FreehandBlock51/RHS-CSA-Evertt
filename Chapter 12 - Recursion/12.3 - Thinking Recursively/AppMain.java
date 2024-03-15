b400f04a9fda2e87dba73db7ff8502ac960ed35f
FreehandBlock51 <freehandblock51@outlook.com>
2024-03-15 09:20:26 -0700

    7.2 lab

--- Chapter 12 - Recursion/12.3 - Thinking Recursively/AppMain.java
+++ Chapter 12 - Recursion/12.3 - Thinking Recursively/AppMain.java
@@ -1,17 +1,44 @@
+import java.util.Objects;
+
 public class AppMain {
+    private static void assertThat(boolean bool, String errMsg) {
+        assert bool;
+        
+        if (!bool) {
+            throw new AssertionError(errMsg);
+        }
+    }
+
+    private static void assertEq(Object a, Object b) {
+        assertThat(Objects.equals(a, a), "Objects.equals(" + Objects.toString(a) + ", " + Objects.toString(a) + ") failed! (a != a)");
+        assertThat(Objects.equals(b, b), "Objects.equals(" + Objects.toString(b) + ", " + Objects.toString(b) + ") failed! (b != b)");
+        assertThat(Objects.equals(a, b), "Objects.equals(" + Objects.toString(a) + ", " + Objects.toString(b) + ") failed! (a != b)");
+        assertThat(Objects.equals(b, a), "Objects.equals(" + Objects.toString(b) + ", " + Objects.toString(a) + ") failed! (b != a)");
+
+        System.out.println("assertion " + Objects.toString(a) + " == " + Objects.toString(b) + " passed!");
+    }
+
     public static void main(String[] args) {
         Practice practice = new Practice();
 
         System.out.println("-------- isPalindrome --------");
-        // TODO: add your test cases here
+        assertEq(practice.isPalindrome("racecar"), true);
+        assertEq(practice.isPalindrome("test"), false);
 
         System.out.println("\n-------- sumDigits --------");
-        // TODO: add your test cases here
+        assertEq(practice.sumDigits(123), 6);
+        assertEq(practice.sumDigits(572), 14);
+        assertEq(practice.sumDigits(116), 8);
 
         System.out.println("\n-------- createString --------");
-        // TODO: add your test cases here
+        assertEq(practice.createString(3), "321.123");
+        assertEq(practice.createString(5), "54321.12345");
+        assertEq(practice.createString(1), "1.1");
+        assertEq(practice.createString(0), "");
         
         System.out.println("\n-------- minValue --------");
-        // TODO: add your test cases here
+        assertEq(practice.minValue(new int[] {-3, 7, -8, 4, 0}, 0), -8);
+        assertEq(practice.minValue(new int[] {8, 7, 1, 10, 6, 9}, 3), 6);
+        assertEq(practice.minValue(new int[] {-1, 0, 1, 2, 3}, 1), 0);
     }
 }
--- Chapter 12 - Recursion/12.3 - Thinking Recursively/Practice.java
+++ Chapter 12 - Recursion/12.3 - Thinking Recursively/Practice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Practice {
     /* Write a palindrome string checker. It should take a String as input and
      *  return a boolean to indicate if it is a palindrome. Note that a palindrome
@@ -9,7 +11,15 @@ public class Practice {
      *  input: isPalindrome("racecar"), output: true
      *  input: isPalindrome("test"), output: false
      */ 
-    // TODO
+    public boolean isPalindrome(String str) {
+        if (str.length() == 0) {
+            return true;
+        }
+        if (!str.substring(str.length() - 1).equals(str.substring(0, 1))) {
+            return false;
+        }
+        return isPalindrome(str.substring(1, str.length() - 1));
+    }
 
 
     /* Write a method to sum the digits of a number. It should take an int and
@@ -21,7 +31,18 @@ public class Practice {
      *  input: sumDigits(572), output: 14
      *  input: sumDigits(116), output: 8
      */
-    // TODO
+    public int sumDigits(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        return sumDigits_aux(n, 0);
+    }
+    private int sumDigits_aux(int n, int curSum) {
+        if (n == 0) {
+            return curSum;
+        }
+        return sumDigits_aux(n / 10, curSum + (n % 10));
+    }
 
     
     /* Write a method that returns a symmetric String that matches the
@@ -34,7 +55,22 @@ public class Practice {
      *  input: createString(1), output: "1.1"
      *  input: createString(0), output: ""
      */
-    // TODO
+    public String createString(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        return createString_aux(n, "");
+    }
+    private String createString_aux(int n, String str) {
+        if (n <= 0) { // make it symmetrical
+            final StringBuilder builder = new StringBuilder(str + ".");
+            for (int i = str.length() - 1; i >= 0; i--) {
+                builder.append(str.substring(i, i + 1));
+            }
+            return builder.toString();
+        }
+        return createString_aux(n - 1, str + n);
+    }
 
 
     /* Write a method that find the smallest number in an array, starting 
@@ -47,5 +83,28 @@ public class Practice {
      *  input: minValue({8, 7, 1, 10, 6, 9}, 3), output: 6
      *  input: minValue({-1, 0, 1, 2, 3}, 1), output: 0
      */
-    // TODO
+    public int minValue(int[] arr, int idx) {
+        try {
+            final int firstMin = arr[idx];
+            return minValue_aux(arr, idx + 1, firstMin)
+        }
+        catch (NullPointerException e) {
+            throw new IllegalArgumentException("Given a null array!");
+        }
+        catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    private int minValue_aux(int[] arr, int curIdx, int curMin) {
+        if (curIdx >= arr.length) {
+            return curMin;
+        }
+        final int curVal = arr[curIdx];
+        if (curVal < curMin) {
+            return minValue_aux(arr, curIdx + 1, curVal);
+        }
+        else {
+            return minValue_aux(arr, curIdx + 1, curMin);
+        }
+    }
 }
