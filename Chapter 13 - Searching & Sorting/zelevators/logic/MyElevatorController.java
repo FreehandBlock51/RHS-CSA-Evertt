01b430ee2198f53bb867eb5fc4ffcd2b222c5cf8
FreehandBlock51 <freehandblock51@outlook.com>
2024-05-13 11:09:43 -0700

    zelevators 1st attempt

--- Chapter 13 - Searching & Sorting/zelevators/logic/MyElevatorController.java
+++ Chapter 13 - Searching & Sorting/zelevators/logic/MyElevatorController.java
@@ -1,7 +1,13 @@
 package logic;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import game.Elevator;
 import game.ElevatorController;
 import game.Game;
+import game.Simulation;
 
 public class MyElevatorController implements ElevatorController {
     // Private member data
@@ -9,15 +15,55 @@ public class MyElevatorController implements ElevatorController {
 
     // Students should implement this function to return their name
     public String getStudentName() {
-        return "<YOUR NAME>";   // <-- TODO: Replace with your name
+        return "Dalton Carter";
     }
     public int getStudentPeriod() {
-        return -1;              // <-- TODO: Replace with your class period
+        return 1;
     }
+
+    private static final class ElevatorRequest {
+        public final Direction direction;
+        public final int floor;
+        public ElevatorRequest(int floor, Direction direction) {
+            this.floor = floor;
+            this.direction = direction;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == null || !(other instanceof ElevatorRequest)) {
+                return false;
+            }
+            ElevatorRequest req = (ElevatorRequest)other;
+            return req.floor == floor && Objects.equals(req.direction, direction);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(floor, direction);
+        }
+    }
+
+    private final ArrayDeque<ElevatorRequest> globalFloorRequestQueue = new ArrayDeque<>();
+    private final ArrayList<ArrayDeque<Integer>> elevatorFloorRequestQueues = new ArrayList<>();
+    private int[] elevatorTargetFloors;
+    private double[] elevatorWaitTimes;
+    private int[] elevatorStressLevels;
+    private static final int MAX_STRESS = 5;
+    private static final double ELEVATOR_WAIT_TIME = 5;
 
     // Event: Game has started
     public void onGameStarted(Game game) {
         this.game = game;
+
+        elevatorTargetFloors = new int[game.getElevatorCount()];
+        elevatorWaitTimes = new double[game.getElevatorCount()];
+        elevatorStressLevels = new int[game.getElevatorCount()];
+
+        // initialize elevator queues
+        for (int i = 0; i < game.getElevatorCount(); i++) {
+            elevatorFloorRequestQueues.add(new ArrayDeque<>());
+        }
     }
 
     // Event: "outside-the-elevator" request, requesting an elevator.
@@ -25,16 +71,27 @@ public class MyElevatorController implements ElevatorController {
     public void onElevatorRequestChanged(int floorIdx, Direction dir, boolean reqEnable) {
         System.out.println("onElevatorRequestChanged(" + floorIdx + ", " + dir + ", " + reqEnable + ")");
 
-        // TODO
-
+        if (reqEnable) {
+            globalFloorRequestQueue.offer(new ElevatorRequest(floorIdx, dir));
+        }
+        else {
+            globalFloorRequestQueue.remove(new ElevatorRequest(floorIdx, dir));
+        }
     }
 
     // Event: "inside-the-elevator" request, requesting to go to a floor.
     //  The event will be triggered with the request is created/enabled & when it is cleared (reqEnable indicates which).
     public void onFloorRequestChanged(int elevatorIdx, int floorIdx, boolean reqEnable) {
-        System.out.println("onFloorRequesteChanged(" + elevatorIdx + ", " + floorIdx + ", " + reqEnable + ")");
+        System.out.println("onFloorRequestChanged(" + elevatorIdx + ", " + floorIdx + ", " + reqEnable + ")");
 
-        // TODO
+        final ArrayDeque<Integer> elevatorQueue = elevatorFloorRequestQueues.get(elevatorIdx);
+
+        if (reqEnable) {
+            elevatorQueue.offer(floorIdx);
+        }
+        else {
+            elevatorQueue.remove(floorIdx);
+        }
 
     }
 
@@ -42,7 +99,17 @@ public class MyElevatorController implements ElevatorController {
     public void onElevatorArrivedAtFloor(int elevatorIdx, int floorIdx, Direction travelDirection) {
         System.out.println("onElevatorArrivedAtFloor(" + elevatorIdx + ", " + floorIdx + ", " + travelDirection + ")");
 
-        // TODO
+        // elevatorFloorRequestQueues.get(elevatorIdx).removeIf(i -> i.intValue() == floorIdx);
+        // globalFloorRequestQueue.removeIf(i -> i.equals(new ElevatorRequest(floorIdx, travelDirection)));
+
+        // if (elevatorTargetFloors[elevatorIdx] == floorIdx) {
+            elevatorWaitTimes[elevatorIdx] = ELEVATOR_WAIT_TIME * (1 + elevatorIdx); // time it takes to travel to the elevator
+        // }
+    }
+
+    public boolean hasOutsideRequestForFloor(int floorIdx) {
+        return game.hasElevatorRequestDown(floorIdx) ||
+            game.hasElevatorRequestUp(floorIdx);
     }
 
     // Event: Called each frame of the simulation (i.e. called continuously)
@@ -51,6 +118,71 @@ public class MyElevatorController implements ElevatorController {
             return;
         }
 
-        // TODO
+        // System.out.println("update()");
+
+        for (int elev = 0; elev < game.getElevatorCount(); elev++) {
+            if (!game.isElevatorIsOnFloor(elev, elevatorTargetFloors[elev]) ||
+             hasOutsideRequestForFloor(elev)) {
+                continue;
+            }
+            else if (elevatorWaitTimes[elev] > 0) { // wait for the zombies to actually enter the elevator
+                elevatorWaitTimes[elev] -= deltaTime;
+                // System.out.println("\televator " + elev + ": " + elevatorWaitTimes[elev] + " seconds remaining until door close");
+                continue;
+            }
+
+            ArrayDeque<Integer> eQ = elevatorFloorRequestQueues.get(elev);
+
+            final int curElev = elev; // for the predicates
+            eQ.removeIf(i -> i.intValue() == elevatorTargetFloors[curElev]);
+            globalFloorRequestQueue.removeIf(r -> r.floor == elevatorTargetFloors[curElev]);
+
+            if (eQ.isEmpty()) {
+                elevatorStressLevels[elev] = 0;
+            }
+            else if (eQ.size() > MAX_STRESS) {
+                elevatorStressLevels[elev] = MAX_STRESS + 1;
+            }
+
+            if (!eQ.isEmpty() && !globalFloorRequestQueue.isEmpty()) {
+                final int primaryTarget = eQ.peek();
+                ElevatorRequest secondaryTarget = globalFloorRequestQueue.peek();
+
+                if (elevatorStressLevels[elev] > MAX_STRESS) {
+                    gotoNextInIndividualQueue(elev);
+                }
+                else if ((secondaryTarget.floor <= game.getElevatorFloor(elev) && primaryTarget <= secondaryTarget.floor) ||
+                (secondaryTarget.floor >= game.getElevatorFloor(elev) && primaryTarget >= secondaryTarget.floor)) {
+                    elevatorStressLevels[elev]++;
+                    gotoNextInGlobalQueue(elev);
+                }
+                else {
+                    gotoNextInIndividualQueue(elev);
+                }
+            }
+            else if (!eQ.isEmpty()) {
+                gotoNextInIndividualQueue(elev);
+            }
+            else if (!globalFloorRequestQueue.isEmpty()) {
+                gotoNextInGlobalQueue(elev);
+            }
+        }
+    }
+
+    @Override
+    public boolean gotoFloor(int elevatorIdx, int floorIdx) {
+        if (game.isElevatorIsHeadingToFloor(elevatorIdx, floorIdx) ||
+        game.isElevatorIsOnFloor(elevatorIdx, floorIdx)) {
+            return true;
+        }
+        elevatorTargetFloors[elevatorIdx] = floorIdx;
+        return ElevatorController.super.gotoFloor(elevatorIdx, floorIdx);
+    }
+
+    private boolean gotoNextInGlobalQueue(int elevatorIdx) {
+        return gotoFloor(elevatorIdx, globalFloorRequestQueue.pop().floor);
+    }
+    private boolean gotoNextInIndividualQueue(int elevatorIdx) {
+        return gotoFloor(elevatorIdx, elevatorFloorRequestQueues.get(elevatorIdx).pop());
     }
 }
