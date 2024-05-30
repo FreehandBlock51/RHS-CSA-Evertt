56dad894edd280538f67ca361bf22ceb3355175a
FreehandBlock51 <freehandblock51@outlook.com>
2024-05-30 11:48:57 -0700

    1st attempt pirate translator

--- Projects/Talk Like a Pirate/PirateConverter.java
+++ Projects/Talk Like a Pirate/PirateConverter.java
@@ -23,20 +23,110 @@
  */
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Scanner;
 
 public class PirateConverter {
+    private static final Map<String, String> pirateMap = new Hashtable<String, String>();
+    static {
+        pirateMap.put("hello", "ahoy");
+        pirateMap.put("my", "me");
+        pirateMap.put("friend", "bucko");
+        pirateMap.put("sir", "matey");
+        pirateMap.put("stranger", "scurvy dog");
+        pirateMap.put("where", "whar");
+        pirateMap.put("is", "be");
+        pirateMap.put("the", "th'");
+        pirateMap.put("you", "ye");
+        pirateMap.put("old", "barnacle covered");
+    }
+
+    private static String translate(String word) {
+        final String loweredWord = word.toLowerCase();
+        if (!pirateMap.containsKey(loweredWord)) {
+            return word;
+        }
+        final String loweredTranslation = pirateMap.get(loweredWord);
+        final String origFirstLetter = word.substring(0, 1);
+        if (origFirstLetter.equals(origFirstLetter.toUpperCase())) {
+            final String transFirstLetter = loweredTranslation.substring(0, 1);
+            final String restOfTrans = loweredTranslation.substring(1);
+            return transFirstLetter.toUpperCase() + restOfTrans;
+        }
+        return loweredTranslation;
+    }
+    private static String translateWithExtra(String untrimmedWord) {
+        final String trimmedWord = untrimmedWord.trim();
+        int wordStart;
+        for (wordStart = 0; wordStart <= untrimmedWord.length(); wordStart++) {
+            if (untrimmedWord.substring(wordStart).startsWith(trimmedWord)) {
+                break;
+            }
+        }
+        final String prefix = untrimmedWord.substring(0, wordStart);
+        final String postfix = untrimmedWord.substring(wordStart + trimmedWord.length());
+
+        final String translatedWord = translate(trimmedWord);
+        return prefix + translatedWord + postfix;
+    }
+    
+    private String txt_en = null;
+    private String txt_pr = null;
 
     public String getEnglishText() {
-        return "TODO";
+        return txt_en;
     }
     public String getPirateText() {
-        return "TODO";
+        return txt_pr;
     }
 
     public boolean fileToPirateTalk(String filename) {
-        // TODO
+        txt_en = null;
+        txt_pr = null;
+
+        final File file = new File(filename);
+        if (!file.canRead()) {
+            return false;
+        }
+        
+        final StringBuilder b_en = new StringBuilder();
+        final StringBuilder b_pr = new StringBuilder("Arr! ");
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                final String line = scanner.nextLine();
+                b_en.append(line);
+                int i = 0;
+                while (i < line.length()) {
+                    int nextSpace = line.indexOf(" ", i);
+                    if (nextSpace == i) {
+                        b_pr.append(" ");
+                        i++;
+                    }
+                    else if (nextSpace < 0) {
+                        final String rawEnd = line.substring(i);
+                        final String translatedEnd = translateWithExtra(rawEnd);
+                        b_pr.append(translatedEnd);
+                        break;
+                    }
+                    else {
+                        final String nextWord = line.substring(i, nextSpace);
+                        final String translatedWord = translateWithExtra(nextWord);
+                        b_pr.append(translatedWord);
+                        i = nextSpace;
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException fnfe) {
+            return false;
+        }
+
+        txt_en = b_en.toString();
+        txt_pr = b_pr.toString();
 
         return true;
     }
