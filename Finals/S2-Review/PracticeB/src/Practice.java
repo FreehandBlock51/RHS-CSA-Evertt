30d1d6764ddad7d4bff235c6636514a088b076b9
FreehandBlock51 <freehandblock51@outlook.com>
2024-05-30 10:10:17 -0700

    Practice B

--- Finals/S2-Review/PracticeB/src/Practice.java
+++ Finals/S2-Review/PracticeB/src/Practice.java
@@ -5,8 +5,18 @@ public class Practice {
           strCatColumn({ { "a", "b", "c" }, { "d", "e", "f" } }, 1 ) -> "be"
      */
     public String strCatColumn(String[][] m, int c) {
-        // TODO
-        return "TODO";
+        if (m == null || c < 0) {
+            return null;
+        }
+
+        final StringBuilder b = new StringBuilder(m.length);
+        for (String[] r : m) {
+            if (c >= r.length) {
+                return null;
+            }
+            b.append(r[c]);
+        }
+        return b.toString();
     }
 
     /* calcSeriesRecursive - write a RECURSIVE method that computes the sum
@@ -15,8 +25,11 @@ public class Practice {
           calcSeriesRecursive(-3,10,12) -> -114
      */
     public int calcSeriesRecursive(int a, int b, int n) {
-        // TODO
-        return 0;
+        return calcSeriesRecursive_aux(a, b, n, 0);
+    }
+    private int calcSeriesRecursive_aux(int a, int b, int n, int s) {
+        if (n <= 0) return s;
+        return calcSeriesRecursive_aux(a, b, n - 1, s + (a * n + b));
     }
 
     /* createFibonacciArray - write a method that returns an array containing
@@ -28,7 +41,37 @@ public class Practice {
           createFibonacciArray(4, 3) -> null
      */
     public int[] createFibonacciArray(int first, int seqCount) {
-        // TODO
-        return null;
+        try {
+            int[] result = null;
+            int prev = 0;
+            int cur = 1;
+            int itemCount = 0;
+            while (itemCount < seqCount) {
+                if (cur == first && itemCount == 0) {
+                    itemCount = 1;
+                    result = new int[seqCount];
+                    result[0] = first;
+                }
+                else if (cur > first && itemCount == 0) {
+                    break;
+                }
+                else if (cur >= first) {
+                    result[itemCount] = cur;
+                    itemCount++;
+                }
+
+                // advance the sequence
+                final int temp = prev;
+                prev = cur;
+                cur += temp;
+            }
+            return result;
+        }
+        catch (NullPointerException npe) {
+            // if we reference null, it's probably our array not being initialized,
+            //  which means that we passed our starting point.  Therefore, the starting
+            //  point isn't part of the sequence, so we return null.
+            return null;
+        }
     }
 }
