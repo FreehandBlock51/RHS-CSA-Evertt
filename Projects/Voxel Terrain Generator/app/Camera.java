544884e17b28595720372ea4c1e45566110c70f5
FreehandBlock51 <freehandblock51@outlook.com>
2024-06-11 09:37:36 -0700

    fixed looking at things

--- Projects/Voxel Terrain Generator/app/Camera.java
+++ Projects/Voxel Terrain Generator/app/Camera.java
@@ -1,6 +1,7 @@
 package app;
 
 import framework.CameraBase;
+import framework.Window;
 import framework.WorldBase;
 import math.Mat3x3;
 import math.Vec2;
@@ -29,12 +30,16 @@ import math.Vec3;
  */
 public class Camera extends CameraBase {
     // consts
+    private static final double MOVE_SPEED = 30;
+    private static final double LOOK_SPEED = 50;
 
     // data
+    private Vec2 prevMouseCursorPos;
 
     // constructor(s)
     public Camera(Vec3 pos, Vec3 forward) {
-        super(pos, forward);
+        super(pos, forward, Vec3.up(), 80, 0.1, 1000.0);
+        prevMouseCursorPos = getMouseCursorPos();
     }
 
     // methods
@@ -42,10 +47,51 @@ public class Camera extends CameraBase {
         // World access
         WorldBase world = WorldBase.get();
 
-        // TODO
+        Vec3 velocity = new Vec3(0, 0, 0);
+        if (isArrowKeyPressed_Down()) {
+            velocity.add(Vec3.multiply(backward(), MOVE_SPEED));
+        }
+        if (isArrowKeyPressed_Up()) {
+            velocity.add(Vec3.multiply(forward(), MOVE_SPEED));
+        }
+        if (isArrowKeyPressed_Left()) {
+            velocity.add(Vec3.multiply(left(), MOVE_SPEED));
+        }
+        if (isArrowKeyPressed_Right()) {
+            velocity.add(Vec3.multiply(right(), MOVE_SPEED));
+        }
+        setPos(Vec3.add(getPos(), Vec3.multiply(velocity, deltaTime)));
     }
+    private static final double TAU = Math.PI * 2;
     private void updateLookDirection(double deltaTime) {
-        // TODO
+        final Window window = Window.get();
+        final Vec2 curMouseCursorPos = getMouseCursorPos();
+        final Vec2 mouseMovement = Vec2.subtract(curMouseCursorPos, prevMouseCursorPos);
+
+        final Vec2 rotation = new Vec2(mouseMovement.x * TAU / window.getWidth(), mouseMovement.y * TAU / window.getHeight());
+        rotation.multiply(Math.PI * 2);
+        Vec3 curLookDir = getLookDir();
+        
+        final Mat3x3 xRotMat = Mat3x3.transRotAxis(Vec3.up(), rotation.x * LOOK_SPEED * deltaTime);
+        final Mat3x3 yRotMat = Mat3x3.transRotAxis(left(), -rotation.y * LOOK_SPEED * deltaTime);
+        setLookDir(
+            xRotMat.transform(
+                yRotMat.transform(
+                    curLookDir
+                )
+            )
+            .unit()
+        );
+        setUpDir(
+            xRotMat.transform(
+                yRotMat.transform(
+                    up()
+                )
+            )
+            .unit()
+        );
+
+        prevMouseCursorPos = curMouseCursorPos;
     }
     public void update(double deltaTime) {
         updateMotion(deltaTime);
